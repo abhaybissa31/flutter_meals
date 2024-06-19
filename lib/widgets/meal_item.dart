89b232901f632d4c7cd3b145c12970ae61452bfa
abhaybissa31 <theabhaybissa@gmail.com>
2024-06-20 00:05:33 +0530

    Completed with meals inside of food category

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/mealmodel.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -10,13 +11,22 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
         onTap: () {},
         child: Stack(
           children: [
             FadeInImage(
                 placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(mealModel.imageUrl)),
+                image: NetworkImage(mealModel.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+                ),
+                
             Positioned(
                 bottom: 0,
                 left: 0,
@@ -41,6 +51,17 @@ class MealItem extends StatelessWidget {
                       ),
                       const SizedBox(
                         height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MealItemTrait(icon: Icons.schedule_rounded, label: '${mealModel.duration} min'),
+                          const SizedBox(width: 12,),
+                          MealItemTrait(icon: Icons.work_history_sharp, label: mealModel.complexity.name[0].toUpperCase()+mealModel.complexity.name.substring(1)),
+                          const SizedBox(width: 12,),
+                          MealItemTrait(icon: Icons.attach_money_sharp, label: mealModel.affordability.name[0].toUpperCase()+mealModel.affordability.name.substring(1)),
+
+                        ],
                       )
                     ],
                   ),
